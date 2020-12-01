8609caecf6522484d467368cc0a2426cf640200a
Arno Balois <abalois@iutbayonne.univ-pau.fr>
2020-12-01 16:51:25 +0100

    Creation de la page dynamique Stage

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,13 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/", name="pro_stage_accueil")
-     */
     public function index(): Response
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return $this->render('pro_stage/index.html.twig');
     }
+     public function entreprises(): Response
+     {
+       return $this->render('pro_stage/entreprises.html.twig');
+     }
+
+      public function formations(): Response
+      {
+        return $this->render('pro_stage/formations.html.twig');
+      }
+
+      public function stages($id): Response
+         {
+           return $this->render('pro_stage/stages.html.twig',['id' => $id]);
+
+         }
+
+
 }
